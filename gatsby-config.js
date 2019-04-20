d3bfa1fa586e14b77c82e1476403c02f22b848e8
Piotr Kwiecinski <piokwiecinski@gmail.com>
2019-04-20 18:13:17 +0100

    feat: add google tag manager and helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,15 @@
+const siteUrl = `https://piotrkwiecinski.com`
+
 module.exports = {
+  siteMetadata: {
+    siteUrl: siteUrl,
+    title: `Piort Kwiecinski`,
+  },
   plugins: [
     `gatsby-plugin-typescript`,
-  ]
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: { id: `GTM-P7K638`, includeInDevelopment: false },
+    },
+  ],
 }
