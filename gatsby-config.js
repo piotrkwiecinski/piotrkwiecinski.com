c3a4e21bcdc0c042d0b472f593b77a4d69745d80
Piotr Kwiecinski <piokwiecinski@gmail.com>
2019-05-26 19:51:50 +0100

    feat: automated deployment to s3 bucket

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,27 @@
-const siteUrl = `https://piotrkwiecinski.com`
+const siteUrl = new URL(`https://piotrkwiecinski.com`)
 
 module.exports = {
   siteMetadata: {
-    siteUrl: siteUrl,
+    siteUrl: siteUrl.href.slice(0, -1),
     title: `Piort Kwiecinski`,
   },
   plugins: [
     `gatsby-plugin-typescript`,
     `gatsby-plugin-robots-txt`,
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: `piotrkwiecinski.com`,
+        hostname: siteUrl.hostname,
+        protocol: siteUrl.protocol.slice(0, -1),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: siteUrl.href.slice(0, -1),
+      },
+    },
     {
       resolve: `gatsby-plugin-google-tagmanager`,
       options: { id: `GTM-P7K638`, includeInDevelopment: false },
