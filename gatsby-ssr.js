16554902ea1dce83e525be5910b94f77a92f1ca5
Piotr Kwiecinski <piokwiecinski@gmail.com>
2019-05-26 22:20:03 +0100

    fix: non unique key prop warning in meta

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,6 +3,7 @@ const React = require('react')
 exports.onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <meta
+      key="google-site-verfication"
       name="google-site-verification"
       content="C16XQgGM1KzwMJJIaMhTqHK1s0sRzIZk9WPYS4Cw4es"
     />,
